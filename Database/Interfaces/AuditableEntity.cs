57d07bb8344cee045f60e61c5f796b96cb2faf88
ZharkovAP <antonzharckov@yandex.ru>
2022-05-21 11:00:04 +0300

    Добавьте файлы проекта.

---
+++ Database/Interfaces/AuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowWorldDbSQLite.Database.Interfaces
+{
+    internal interface AuditableEntity
+    {
+        string CreatedBy { get; set; }
+        string UpdatedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+        DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ Database/_ContextDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WindowWorldDbSQLite.Database.Models;
+
+namespace WindowWorldDbSQLite.Database
+{
+    public class _ContextDb: DBContext
+    {
+
+    }
+}
